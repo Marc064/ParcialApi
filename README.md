ec461b633e3bb7362679dfe7f2dd74c18dd49b24
Marc064 <95501065+Marc064@users.noreply.github.com>
2023-12-14 18:33:39 -0500

    Consulta de datos

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ParcialApi
  
+## Autor
+
+Marco Antonio Vargas Garcia - 202015214
+
+## Descripción del proyecto
+
+Se realizo el consumo de una api para realizar peticiones de tipo GET, POST y DELETE
+
+## Puerto
+
+Se usa el puerto 3000
+
+## Instalacion de dependencias
+
+Para la instalacion de dependencias se debe usar el siguiente comando en la consola, en la carpeta del proyecto
+
+```bash
+npm install 
+```
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,48 @@
 function loadData() {
     return new Promise((res, rej) => {
-        fetch("")
+        fetch("https://api-dishes.vercel.app/")
             .then(result => result.json())
             .then(result => res(result))
             .catch(err => rej(err))
     })
 }
+
+loadData()
+    .then( result => {
+        if (result.state) {
+            try {
+                const table = document.getElementById('tBody')
+                table.innerHTML=''
+                result.data.forEach(dish => {
+                    const row = document.createElement('tr')
+                    const colId = document.createElement('td')
+                    colId.appendChild(document.createTextNode(dish.idDish))
+                    row.append(colId)
+                    const colName = document.createElement('td')
+                    colName.appendChild(document.createTextNode(dish.name))
+                    row.append(colName)
+                    const colCalories = document.createElement('td')
+                    colCalories.appendChild(document.createTextNode(dish.calories))
+                    row.append(colCalories)
+                    const colVegetarian = document.createElement('td')
+                    let vegetarian = dish.isVegetarian ? "Si":"No"
+                    colVegetarian.appendChild(document.createTextNode(vegetarian))
+                    row.append(colVegetarian)
+                    const colValue = document.createElement('td')
+                    colValue.appendChild(document.createTextNode(dish.value))
+                    row.append(colValue)
+                    const colComments = document.createElement('td')
+                    colComments.appendChild(document.createTextNode(dish.comments))
+                    row.append(colComments)
+                    table.append(row)
+                })
+            } catch (err) {
+                alert("Ha ocurrido un error")
+                console.log(err)
+            }
+        }
+    })
+    .catch( err => {
+        alert("Ha ocurrido un error")
+        console.log(err)
+    })
